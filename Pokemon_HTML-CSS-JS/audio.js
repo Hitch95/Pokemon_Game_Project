e3dc1baf9f54db8b4281c7513fd81abf6e41d059
Hitch95 <65714371+Hitch95@users.noreply.github.com>
2022-08-24 21:53:04 +0200

    first commit

---
+++ Pokemon_HTML-CSS-JS/audio.js
@@ -0,0 +1,37 @@
+const audio = {
+  Map: new Howl({
+    src: "./AUDIO.files/map.wav",
+    html5: true,
+    volume: 0.1,
+  }),
+  InitBattle: new Howl({
+    src: "./AUDIO.files/initBattle.wav",
+    html5: true,
+    volume: 0.1,
+  }),
+  Battle: new Howl({
+    src: "./AUDIO.files/battle.mp3",
+    html5: true,
+    volume: 0.1,
+  }),
+  tackleHit: new Howl({
+    src: "./AUDIO.files/tackleHit.wav",
+    html5: true,
+    volume: 0.1,
+  }),
+  fireballHit: new Howl({
+    src: "./AUDIO.files/fireballHit.wav",
+    html5: true,
+    volume: 0.1,
+  }),
+  initFireball: new Howl({
+    src: "./AUDIO.files/initFireball.wav",
+    html5: true,
+    volume: 0.1,
+  }),
+  victory: new Howl({
+    src: "./AUDIO.files/victory.wav",
+    html5: true,
+    volume: 0.1
+  })
+};
